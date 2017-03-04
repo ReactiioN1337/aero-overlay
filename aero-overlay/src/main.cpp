41a5bb85c822bfde5bb7b3bdb111b4679930ea18
Reactiion1337 <reactiion@haze-productions.com>
2017-03-04 23:39:06 +0100

    Testing new changes :)

--- aero-overlay/src/main.cpp
+++ aero-overlay/src/main.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
+#include <render/Device2D.hpp>
+#include <render/Device3D9.hpp>
+using namespace render;
 
 int32_t main()
 {
-    std::cout << "repository init" << std::endl;
+    auto overlay = Overlay::New( EDeviceType::Direct2D );
+    if( !overlay ) {
+        return EXIT_FAILURE;
+    }
+
+    if( !overlay->create( "Counter-Strike: Global Offensive" ) ) {
+        return EXIT_FAILURE;
+    }
+
+    while( overlay->render() ) {
+        
+        std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) );
+    }
+
+    overlay->shutdown();
     return EXIT_SUCCESS;
 }
