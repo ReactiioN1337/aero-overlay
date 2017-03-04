bc6ab2b5691de16d45f7396d1b75e9bca5fbe53b
Reactiion1337 <reactiion@haze-productions.com>
2017-03-04 23:36:07 +0100

    Added some operators, changed some method logics

--- aero-overlay/include/drawing/Color.hpp
+++ aero-overlay/include/drawing/Color.hpp
@@ -15,7 +15,7 @@ public:
     /// @author ReactiioN
     /// @date   04.03.2017
     ///-------------------------------------------------------------------------------------------------
-    Color() = default;
+    Color();
 
     ///-------------------------------------------------------------------------------------------------
     /// Constructor.
@@ -23,16 +23,9 @@ public:
     /// @author ReactiioN
     /// @date   04.03.2017
     ///
-    /// @param  r   The int32_t to process.
-    /// @param  g   The int32_t to process.
-    /// @param  b   The int32_t to process.
-    /// @param  a   (Optional) The int32_t to process.
+    /// @param  color   The color.
     ///-------------------------------------------------------------------------------------------------
-    Color(
-        const int32_t r,
-        const int32_t g,
-        const int32_t b,
-        const int32_t a = 255 );
+    Color( const Color_t& color );
 
     ///-------------------------------------------------------------------------------------------------
     /// Constructor.
@@ -40,9 +33,9 @@ public:
     /// @author ReactiioN
     /// @date   04.03.2017
     ///
-    /// @param  hex_color   The hexadecimal color.
+    /// @param  color   The color.
     ///-------------------------------------------------------------------------------------------------
-    Color( const uint32_t hex_color );
+    Color( const uint32_t color );
 
     ///-------------------------------------------------------------------------------------------------
     /// Assignment operator.
@@ -50,11 +43,71 @@ public:
     /// @author ReactiioN
     /// @date   04.03.2017
     ///
-    /// @param  other   The other.
+    /// @param  color   The color.
     ///
     /// @return A shallow copy of this object.
     ///-------------------------------------------------------------------------------------------------
-    Color&                operator = ( const Color& other );
+    Color&         operator = ( const Color& color );
+
+    ///-------------------------------------------------------------------------------------------------
+    /// Assignment operator.
+    ///
+    /// @author ReactiioN
+    /// @date   04.03.2017
+    ///
+    /// @param  color   The color.
+    ///
+    /// @return A shallow copy of this object.
+    ///-------------------------------------------------------------------------------------------------
+    Color&         operator = ( const uint32_t color );
+
+    ///-------------------------------------------------------------------------------------------------
+    /// Equality operator.
+    ///
+    /// @author ReactiioN
+    /// @date   04.03.2017
+    ///
+    /// @param  color   The color.
+    ///
+    /// @return True if the parameters are considered equivalent.
+    ///-------------------------------------------------------------------------------------------------
+    bool           operator == ( const Color& color ) const;
+
+    ///-------------------------------------------------------------------------------------------------
+    /// Equality operator.
+    ///
+    /// @author ReactiioN
+    /// @date   04.03.2017
+    ///
+    /// @param  color   The color.
+    ///
+    /// @return True if the parameters are considered equivalent.
+    ///-------------------------------------------------------------------------------------------------
+    bool           operator == ( const uint32_t color ) const;
+
+    ///-------------------------------------------------------------------------------------------------
+    /// Inequality operator.
+    ///
+    /// @author ReactiioN
+    /// @date   04.03.2017
+    ///
+    /// @param  color   The color.
+    ///
+    /// @return True if the parameters are not considered equivalent.
+    ///-------------------------------------------------------------------------------------------------
+    bool           operator != ( const Color& color ) const;
+
+    ///-------------------------------------------------------------------------------------------------
+    /// Inequality operator.
+    ///
+    /// @author ReactiioN
+    /// @date   04.03.2017
+    ///
+    /// @param  color   The color.
+    ///
+    /// @return True if the parameters are not considered equivalent.
+    ///-------------------------------------------------------------------------------------------------
+    bool           operator != ( const uint32_t color ) const;
 
     ///-------------------------------------------------------------------------------------------------
     /// Array indexer operator.
@@ -66,7 +119,7 @@ public:
     ///
     /// @return The indexed value.
     ///-------------------------------------------------------------------------------------------------
-    uint8_t&              operator [] ( const size_t index );
+    uint8_t&       operator [] ( const size_t index );
 
     ///-------------------------------------------------------------------------------------------------
     /// Array indexer operator.
@@ -78,7 +131,7 @@ public:
     ///
     /// @return The indexed value.
     ///-------------------------------------------------------------------------------------------------
-    const uint8_t&        operator [] ( const size_t index ) const;
+    const uint8_t& operator [] ( const size_t index ) const;
 
     ///-------------------------------------------------------------------------------------------------
     /// Function call operator.
@@ -88,7 +141,7 @@ public:
     ///
     /// @return The result of the operation.
     ///-------------------------------------------------------------------------------------------------
-    uint8_t&              operator () ( const size_t index );
+    uint8_t&       operator () ( const size_t index );
 
     ///-------------------------------------------------------------------------------------------------
     /// Function call operator.
@@ -98,7 +151,7 @@ public:
     ///
     /// @return The result of the operation.
     ///-------------------------------------------------------------------------------------------------
-    const uint8_t&        operator () ( const size_t index ) const;
+    const uint8_t& operator () ( const size_t index ) const;
 
     ///-------------------------------------------------------------------------------------------------
     /// Ats the given index.
@@ -110,7 +163,7 @@ public:
     ///
     /// @return A reference to an uint8_t.
     ///-------------------------------------------------------------------------------------------------
-    inline uint8_t&       at( const size_t index );
+    uint8_t&       at( const size_t index );
 
     ///-------------------------------------------------------------------------------------------------
     /// Ats the given index.
@@ -122,82 +175,145 @@ public:
     ///
     /// @return A reference to a const uint8_t.
     ///-------------------------------------------------------------------------------------------------
-    inline const uint8_t& at( const size_t index ) const;
+    const uint8_t& at( const size_t index ) const;
 
     ///-------------------------------------------------------------------------------------------------
-    /// Converts this object to a hexadecimal.
+    /// Addition operator.
     ///
     /// @author ReactiioN
     /// @date   04.03.2017
     ///
-    /// @return This object as an uint32_t.
+    /// @param  color   The color.
+    ///
+    /// @return The result of the operation.
     ///-------------------------------------------------------------------------------------------------
-    uint32_t              to_hex() const;
+    Color          operator + ( const Color& color );
 
     ///-------------------------------------------------------------------------------------------------
-    /// Sets the rgba channel.
+    /// Addition assignment operator.
     ///
     /// @author ReactiioN
     /// @date   04.03.2017
     ///
-    /// @param  r   The int32_t to process.
-    /// @param  g   The int32_t to process.
-    /// @param  b   The int32_t to process.
-    /// @param  a   (Optional) The int32_t to process.
+    /// @param  color   The color.
+    ///
+    /// @return The result of the operation.
     ///-------------------------------------------------------------------------------------------------
-    void                  set(
-        const int32_t r,
-        const int32_t g,
-        const int32_t b,
-        const int32_t a = 255 );
+    Color&         operator += ( const Color& color );
 
     ///-------------------------------------------------------------------------------------------------
-    /// Sets the given hexadecimal color.
+    /// Subtraction operator.
     ///
     /// @author ReactiioN
     /// @date   04.03.2017
     ///
-    /// @param  hex_color   The Hexadecimal color to set.
+    /// @param  color   The color.
+    ///
+    /// @return The result of the operation.
     ///-------------------------------------------------------------------------------------------------
-    void                  set( const uint32_t hex_color );
+    Color          operator - ( const Color& color );
 
     ///-------------------------------------------------------------------------------------------------
-    /// Gets red channel.
+    /// Subtraction assignment operator.
+    ///
+    /// @author ReactiioN
+    /// @date   04.03.2017
+    ///
+    /// @param  color   The color.
+    ///
+    /// @return The result of the operation.
+    ///-------------------------------------------------------------------------------------------------
+    Color&         operator -= ( const Color& color );
+
+    ///-------------------------------------------------------------------------------------------------
+    /// Gets the color code as hexadecimal.
+    ///
+    /// @author ReactiioN
+    /// @date   04.03.2017
+    ///
+    /// @return An uint32_t.
+    ///-------------------------------------------------------------------------------------------------
+    uint32_t       to_hex() const;
+
+    ///-------------------------------------------------------------------------------------------------
+    /// Sets the given color as hex deciam.
+    ///
+    /// @author ReactiioN
+    /// @date   04.03.2017
+    ///
+    /// @param  color   The color to set.
+    ///-------------------------------------------------------------------------------------------------
+    void           set( const uint32_t color );
+
+    ///-------------------------------------------------------------------------------------------------
+    /// Sets the RGBA channel.
+    ///
+    /// @author ReactiioN
+    /// @date   04.03.2017
+    ///
+    /// @tparam T   Generic type parameter.
+    /// @param  R   The Red channel.
+    /// @param  G   The Green channel
+    /// @param  B   The Blue channel.
+    /// @param  A   (Optional) The Alpha channel.
+    ///-------------------------------------------------------------------------------------------------
+    template<typename T>
+    void           set( const T R, const T G, const T B, const T A = static_cast<T>( 255 ) );
+
+    ///-------------------------------------------------------------------------------------------------
+    /// Constructor.
+    ///
+    /// @author ReactiioN
+    /// @date   04.03.2017
+    ///
+    /// @tparam T   Generic type parameter.
+    /// @param  R   The T to process.
+    /// @param  G   The T to process.
+    /// @param  B   The T to process.
+    /// @param  A   (Optional) The T to process.
+    ///-------------------------------------------------------------------------------------------------
+    template<typename T>
+    Color( const T R, const T G, const T B, const T A = static_cast<T>( 255 ) );
+
+    ///-------------------------------------------------------------------------------------------------
+    /// Gets the red channel.
     ///
     /// @author ReactiioN
     /// @date   04.03.2017
     ///
     /// @tparam T   Generic type parameter.
     ///
-    /// @return A T.
+    /// @return The red channel as type T.
     ///-------------------------------------------------------------------------------------------------
     template<typename T = int32_t>
-    T                     r() const;
+    T r() const;
 
     ///-------------------------------------------------------------------------------------------------
-    /// Gets green channel.
+    /// Gets the green channel.
     ///
     /// @author ReactiioN
     /// @date   04.03.2017
     ///
     /// @tparam T   Generic type parameter.
     ///
-    /// @return A T.
+    /// @return The green channel as type T.
     ///-------------------------------------------------------------------------------------------------
     template<typename T = int32_t>
-    T                     g() const;
+    T g() const;
+
     ///-------------------------------------------------------------------------------------------------
-    /// Gets blue channel.
+    /// Gets the blue channel.
     ///
     /// @author ReactiioN
     /// @date   04.03.2017
     ///
     /// @tparam T   Generic type parameter.
     ///
-    /// @return A T.
+    /// @return The blue channel as type T.
     ///-------------------------------------------------------------------------------------------------
+
     template<typename T = int32_t>
-    T                     b() const;
+    T b() const;
 
     ///-------------------------------------------------------------------------------------------------
     /// Gets alpha channel.
@@ -207,67 +323,61 @@ public:
     ///
     /// @tparam T   Generic type parameter.
     ///
-    /// @return A T.
+    /// @return The alpha channel as type T.
     ///-------------------------------------------------------------------------------------------------
+
     template<typename T = int32_t>
-    T                     a() const;
+    T a() const;
 
 private:
-    /// The color.
     Color_t m_Color;
 };
 
-
-inline uint8_t& Color::at(
-    const size_t index )
+template<typename T>
+Color::Color( const T R, const T G, const T B, const T A )
 {
-    return m_Color.at( 
-        std::clamp( 
-            index,
-            static_cast<size_t>( 0 ),
-            static_cast<size_t>( 1 )
-        ) 
-    );
+    set( R, G, B, A );
 }
 
-inline const uint8_t& Color::at(
-    const size_t index ) const
+template<typename T>
+void Color::set( const T R, const T G, const T B, const T A )
 {
-    return m_Color.at(
-        std::clamp(
-            index,
-            static_cast<size_t>( 0 ),
-            static_cast<size_t>( 1 )
-        )
-    );
+    static_assert( std::is_arithmetic<T>::value, "Type T has to be arithmetic" );
+
+    const auto mins = static_cast<T>( 0 );
+    const auto maxs = static_cast<T>( 255 );
+    at( 0 ) = static_cast<uint8_t>( std::clamp( R, mins, maxs ) );
+    at( 1 ) = static_cast<uint8_t>( std::clamp( G, mins, maxs ) );
+    at( 2 ) = static_cast<uint8_t>( std::clamp( B, mins, maxs ) );
+    at( 3 ) = static_cast<uint8_t>( std::clamp( A, mins, maxs ) );
 }
 
 template<typename T>
 T Color::r() const
 {
-    static_assert( std::is_arithmetic<T>::value, "Type T has to be arithmetic!" );
-    return static_cast<T>( m_Color.at( 0 ) );
+    static_assert( std::is_arithmetic<T>::value, "Type T has to be arithmetic" );
+    return static_cast<T>( at( 0 ) );
 }
 
 template<typename T>
 T Color::g() const
 {
-    static_assert( std::is_arithmetic<T>::value, "Type T has to be arithmetic!" );
-    return static_cast<T>( m_Color.at( 1 ) );
+    static_assert( std::is_arithmetic<T>::value, "Type T has to be arithmetic" );
+    return static_cast<T>( at( 1 ) );
 }
 
 template<typename T>
 T Color::b() const
 {
-    static_assert( std::is_arithmetic<T>::value, "Type T has to be arithmetic!" );
-    return static_cast<T>( m_Color.at( 2 ) );
+    static_assert( std::is_arithmetic<T>::value, "Type T has to be arithmetic" );
+    return static_cast<T>( at( 2 ) );
 }
 
 template<typename T>
 T Color::a() const
 {
-    static_assert( std::is_arithmetic<T>::value, "Type T has to be arithmetic!" );
-    return static_cast<T>( m_Color.at( 3 ) );
+    static_assert( std::is_arithmetic<T>::value, "Type T has to be arithmetic" );
+    return static_cast<T>( at( 3 ) );
 }
 
 }
--- aero-overlay/src/Color.cpp
+++ aero-overlay/src/Color.cpp
@@ -1,93 +1,135 @@
 #include <drawing/Color.hpp>
 using namespace drawing;
 
-Color::Color( 
-    const int32_t r,
-    const int32_t g,
-    const int32_t b,
-    const int32_t a )
+Color::Color()
 {
-    set( r, g, b, a );
+    m_Color.fill( 255 );
 }
 
-Color::Color(
-    const uint32_t hex_color )
+Color::Color( const Color_t& color ) :
+    m_Color( color )
 {
-    set( hex_color );
 }
 
-Color& Color::operator = (
-    const Color& other )
+Color::Color( const uint32_t color )
+{
+    set( color );
+}
+
+Color& Color::operator = ( const Color& color )
+{
+    set( color.to_hex() );
+    return *this;
+}
+
+Color& Color::operator = ( const uint32_t color )
+{
+    set( color );
+    return *this;
+}
+
+bool Color::operator == ( const Color& color ) const
+{
+    return to_hex() == color.to_hex();
+}
+
+bool Color::operator == ( const uint32_t color ) const
+{
+    return to_hex() == color;
+}
+
+bool Color::operator != ( const Color& color ) const
+{
+    return !( *this == color );
+}
+
+bool Color::operator != ( const uint32_t  color ) const
+{
+    return !( *this == color );
+}
+
+uint8_t& Color::operator [] ( const size_t index )
+{
+    return m_Color[ index ];
+}
+
+const uint8_t& Color::operator [] ( const size_t index ) const
+{
+    return m_Color[ index ];
+}
+
+uint8_t& Color::operator () ( const size_t index )
+{
+    return at( index );
+}
+
+const uint8_t& Color::operator () ( const size_t index ) const
+{
+    return at( index );
+}
+
+uint8_t& Color::at( const size_t index )
+{
+    return m_Color.at( std::clamp( index,
+                                   static_cast<size_t>( 0 ),
+                                   static_cast<size_t>( 3 ) )
+    );
+}
+
+const uint8_t& Color::at( const size_t index ) const
+{
+    return m_Color.at( std::clamp( index,
+                                   static_cast<size_t>( 0 ),
+                                   static_cast<size_t>( 3 ) )
+    );
+}
+
+Color Color::operator + ( const Color& color )
+{
+    Color_t buf;
+    for( size_t i = 0; i < 4; ++i ) {
+        buf.at( i ) = std::clamp( at( i ) + color( i ), 0, 255 );
+    }
+    return Color( buf );
+}
+
+Color& Color::operator += ( const Color& color )
 {
     for( size_t i = 0; i < 4; ++i ) {
-        at( 0 ) = other( i );
+        at( i ) = std::clamp( at( i ) + color( i ), 0, 255 );
     }
     return *this;
 }
 
-uint8_t& Color::operator [] (
-    const size_t index )
+Color Color::operator - ( const Color& color )
 {
-    return m_Color.at(
-        std::clamp(
-            index,
-            static_cast<size_t>( 0 ),
-            static_cast<size_t>( 1 )
-        )
-    );
+    Color_t buf;
+    for( size_t i = 0; i < 4; ++i ) {
+        buf.at( i ) = std::clamp( at( i ) - color( i ), 0, 255 );
+    }
+    return Color( buf );
 }
 
-const uint8_t& Color::operator [] (
-    const size_t index ) const
+Color& Color::operator -= ( const Color& color )
 {
-    return m_Color.at(
-        std::clamp(
-            index,
-            static_cast<size_t>( 0 ),
-            static_cast<size_t>( 1 )
-        )
-    );
-}
-
-uint8_t& Color::operator () (
-    const size_t index )
-{
-    return at( index );
-}
-
-const uint8_t& Color::operator () (
-    const size_t index ) const
-{
-    return at( index );
+    for( size_t i = 0; i < 4; ++i ) {
+        at( i ) = std::clamp( at( i ) - color( i ), 0, 255 );
+    }
+    return *this;
 }
 
 uint32_t Color::to_hex() const
 {
-    return static_cast<uint32_t>(
-        at( 3 ) << 24 | /// a
-        at( 0 ) << 16 | /// r
-        at( 1 ) <<  8 | /// g
-        at( 2 )         /// b
-    );
+    return static_cast<uint32_t>( at( 3 ) << 24 |
+                                  at( 0 ) << 16 |
+                                  at( 1 ) <<  8 |
+                                  at( 2 ) );
 }
 
-void Color::set(
-    const int32_t r,
-    const int32_t g,
-    const int32_t b,
-    const int32_t a )
+void Color::set( const uint32_t color )
 {
-    at( 0 ) = static_cast<uint8_t>( std::clamp( r, 0, 255 ) );
-    at( 1 ) = static_cast<uint8_t>( std::clamp( g, 0, 255 ) );
-    at( 2 ) = static_cast<uint8_t>( std::clamp( b, 0, 255 ) );
-    at( 3 ) = static_cast<uint8_t>( std::clamp( a, 0, 255 ) );
-}
-
-void Color::set(
-    const uint32_t hex_color )
-{
-    at( 3 ) = static_cast<uint8_t>( hex_color >> 24 & 0xFF );
-    at( 0 ) = static_cast<uint8_t>( hex_color >> 16 & 0xFF );
-    at( 1 ) = static_cast<uint8_t>( hex_color >>  8 & 0xFF );
-    at( 2 ) = static_cast<uint8_t>( hex_color & 0xFF );
+    at( 3 ) = static_cast<uint8_t>( color >> 24 & 0xFF );
+    at( 0 ) = static_cast<uint8_t>( color >> 16 & 0xFF );
+    at( 1 ) = static_cast<uint8_t>( color >>  8 & 0xFF );
+    at( 2 ) = static_cast<uint8_t>( color       & 0xFF );
 }
