e9b529f34c0ddc47dda35be2a6a6425d9cca2318
Reactiion1337 <reactiion@haze-productions.com>
2017-03-05 02:27:37 +0100

    Added example

--- aero-overlay/src/main.cpp
+++ aero-overlay/src/main.cpp
@@ -2,14 +2,74 @@
 #include <render/Device3D9.hpp>
 using namespace render;
 
-int32_t main()
+std::unique_ptr<Overlay> create_overlay( const EDeviceType type, const std::string& window_title )
 {
-    auto overlay = Overlay::New( EDeviceType::Direct2D );
+    auto overlay = Overlay::New( type );
     if( !overlay ) {
-        return EXIT_FAILURE;
+        return nullptr;
+    }
+    if( !overlay->create( window_title ) ) {
+        return nullptr;
     }
 
-    if( !overlay->create( "Counter-Strike: Global Offensive" ) ) {
+    auto surface = overlay->get_surface();
+    if( !surface ) {
+        return nullptr;
+    }
+
+    const auto is_d3d9 = type == EDeviceType::Direct3D9;
+
+    if( !surface->add_font(
+        "default",
+        "Segoe UI",
+        is_d3d9 ? 14 : 12,
+        is_d3d9 ? FW_NORMAL : DWRITE_FONT_WEIGHT_NORMAL,
+        /// or DEFAULT_QUALITY instead of PROOF_QUALITY for anti aliasing
+        is_d3d9 ? PROOF_QUALITY : DWRITE_FONT_STRETCH_NORMAL
+    ) ) {
+        return nullptr;
+    }
+
+    return std::move( overlay );
+}
+
+int32_t main( int32_t argc, char** argv )
+{
+    auto print_helper = []{
+        std::cout << "usage: executable.exe --device \"d2d or d3d9\" --window \"window title\"" << std::endl;
+        return EXIT_FAILURE;
+    };    
+
+    if( argc != 5 ) {
+        return print_helper();
+    }
+
+    const std::string device_cmd( argv[ 1 ] );
+    const std::string device_key( argv[ 2 ] );
+    const std::string window_cmd( argv[ 3 ] );
+    const std::string window_key( argv[ 4 ] );
+    if( device_cmd.compare( "--device" ) != 0 ||
+        window_cmd.compare( "--window" ) != 0 ) {
+        return print_helper();
+    }
+
+    std::unique_ptr<Overlay> overlay = nullptr;
+    if( !device_key.compare( "d2d" ) ) {
+        overlay = create_overlay( EDeviceType::Direct2D, window_key );
+    }
+    else if( !device_key.compare( "d3d9" ) ) {
+        overlay = create_overlay( EDeviceType::Direct3D9, window_key );
+    }
+    else {
+        return print_helper();
+    }
+
+    auto* callback = overlay->add_callback(
+        "test", []( Surface* surface ) {
+            surface->text( 5, 5, "default", 0xFFFFFFFF, "Aero Overlay by ReactiioN" );
+       }
+    );
+    if( !callback ) {
         return EXIT_FAILURE;
     }
 
