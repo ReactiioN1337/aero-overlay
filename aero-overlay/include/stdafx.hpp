5ab11a91497b63da968e7b8b11cad891c1ab99ec
ReactiioN1337 <reactiion@haze-productions.com>
2017-04-05 21:04:33 +0200

    Removed own implemention of std::clamp

--- aero-overlay/include/stdafx.hpp
+++ aero-overlay/include/stdafx.hpp
@@ -3,10 +3,8 @@
 #define REACTIION_AERO_OVERLAY_HPP
 #include <Windows.h>
 
-
 #include <dwmapi.h>
 #pragma comment( lib, "dwmapi.lib" )
-
 
 /// std::arry
 #include <array>
@@ -32,39 +30,6 @@
 #include <thread>
 /// std::vector
 #include <vector>
-
-#if _MSC_VER < 1910 /// Visual Studio 2017(RC)
-namespace std {
-///-------------------------------------------------------------------------------------------------
-/// Clamp the given in.
-///
-/// @author ReactiioN
-/// @date   04.03.2017
-///
-/// @tparam T   Generic type parameter.
-/// @param  in      The in.
-/// @param  mins    The mins.
-/// @param  maxs    The maxs.
-///
-/// @return A reference to a const T.
-///-------------------------------------------------------------------------------------------------
-#if _MSC_VER == 1900 /// Visual Studio 2015
-template<typename T>
-constexpr const T& clamp( const T& in, const T& mins, const T& maxs )
-{
-    static_assert( std::is_arithmetic<T>::value, "Type T has to be arithmetic" );
-    return std::min( std::max( in, mins ), maxs );
-}
-#else /// Visual Studio 2013 or older, upgrade your vs bro
-template<typename T>
-const T& clamp( const T& in, const T& mins, const T& maxs )
-{
-    static_assert( std::is_arithmetic<T>::value, "Type T has to be arithmetic" );
-    return std::min( std::max( in, mins ), maxs );
-}
-#endif
-}
-#endif
 
 ///-------------------------------------------------------------------------------------------------
 /// Safe release.
