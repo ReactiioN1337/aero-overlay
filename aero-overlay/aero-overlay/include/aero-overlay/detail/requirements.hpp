323a0a550286172eb8abadec7ff8df32eeeb581b
ReactiioN <reactiion@haze-productions.com>
2019-11-20 16:57:27 +0100

    🎨 Added lib requirements

---
+++ aero-overlay/aero-overlay/include/aero-overlay/detail/requirements.hpp
@@ -0,0 +1,20 @@
+///--------------------------------------------------------------------------------
+///-- Author        ReactiioN
+///-- Copyright     2016-2019, ReactiioN
+///-- License       MIT
+///--------------------------------------------------------------------------------
+#pragma once
+#if !defined(_CRT_SECURE_NO_WARNINGS)
+    #define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#if !defined(NOMINMAX)
+    #define NOMINMAX
+#endif
+
+#include <Windows.h>
+#include <array>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
