b70198e0c8ecbd63ff338d473f04b6d45eed7443
ReactiioN <reactiion@haze-productions.com>
2020-05-24 03:31:51 +0200

    📦 Added Direct2D and DirectWrite includes

---
+++ src/direct2d/pre_include.hpp
@@ -0,0 +1,40 @@
+///--------------------------------------------------------------------------------
+///-- Author        ReactiioN
+///-- Copyright     2016-2019, ReactiioN
+///-- License       MIT
+///--------------------------------------------------------------------------------
+#pragma once
+
+#include <aero-overlay/core/requirements.hpp>
+
+#if !defined(_D2D1_H_)
+    #include <d2d1.h>
+    #include <d2d1helper.h>
+    #include <dwrite.h>
+#endif
+#pragma comment(lib, "d2d1.lib")
+#pragma comment(lib, "dwrite.lib")
+
+#include <aero-overlay/surface.hpp>
+
+namespace aero::detail {
+template<typename type>
+void safe_release(
+    type** data
+)
+{
+    static_assert( std::is_base_of_v<IUnknown, type> );
+    if( data && *data ) {
+        ( *data )->Release();
+        ( *data ) = nullptr;
+    }
+}
+
+constexpr float round_float(
+    const float value
+)
+{
+    return static_cast<float>( static_cast<std::int32_t>( value ) );
+}
+
+}
